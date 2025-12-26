d69fb51497c744d84fd72c2d162b602f36878384
Oleg <olegmatrenitsky@gmail.com>
2025-12-26 15:36:52 +0000

    modified:   scripts/run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,4 +2,18 @@
 set -e
 
 echo "Запуск приложения."
-go run main.go
+go run main.go &
+
+# Проверка запуска
+
+for i in {1..10}; do
+  if curl -s http://localhost:8080 &> /dev/null; then
+    echo "Сервер готов к работе."
+    exit 0
+  fi
+  echo "Сервер пока не готов..."
+  sleep 1
+done
+
+echo "Ошибка: сервер не запустился вовремя."
+exit 1
